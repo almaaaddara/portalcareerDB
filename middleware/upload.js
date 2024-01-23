350c6c34e4bbca8d288473d5fc304220c6146f44
almaaaddara <almaditha22@gmail.com>
2024-01-23 21:27:41 +0800

    init multer

---
+++ middleware/upload.js
@@ -0,0 +1,16 @@
+const multer = require('multer');
+const ApiError = require('../../utils/apiError');
+
+const storage = multer.memoryStorage();
+const allowedMimeType = ['image/jpg', 'image/png', 'image/jpeg', 'application/pdf'];
+
+const fileFilter = (req, file, cb) => {
+  const isAllowedMimeType = allowedMimeType.includes(file.mimetype);
+
+  if (isAllowedMimeType) {
+    return cb(null, true);
+  }
+  return cb(new ApiError('Ekstensi gambar tidak valid', 400));
+};
+
+module.exports = multer({ storage, fileFilter });
